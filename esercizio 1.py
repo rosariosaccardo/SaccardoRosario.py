9e765ce9feebf99be66940ce2e6ed82aa9a58c70
rosariosaccardo <75366888+rosariosaccardo@users.noreply.github.com>
2021-01-02 18:10:58 +0100

    Add files via upload

---
+++ esercizio 1.py
@@ -0,0 +1,18 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> giorno1=45.00
+>>> giorno2=80.00
+>>> giorno3=120.00
+>>> giorno4=160.00
+>>> costogiornalieroextra=40.00
+>>> numerogiorni=4
+>>> costogiornalieroextra*numerogiorni
+160.0
+>>> a=giorno1
+>>> b=giorno2
+>>> c=giorno3
+>>> d=giorno4
+>>> extra=costogiornalieroextra*numerogiorni
+>>> a+b+c+d+extra
+565.0
+>>> 
