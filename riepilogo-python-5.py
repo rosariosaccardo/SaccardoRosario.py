d6820f0a6bf023cdac5aba47f373d4b6600b1869
rosariosaccardo <75366888+rosariosaccardo@users.noreply.github.com>
2021-02-25 10:05:15 +0100

    Add files via upload

---
+++ riepilogo-python-5.py
@@ -0,0 +1,10 @@
+colore=input("digita un colore")
+if colore == "rosso":
+    print("bello")
+elif colore == "ROSSO":
+    print("bello")
+elif colore == "Rosso":
+    print("bello")
+else:
+    print("non mi piace")
+    
